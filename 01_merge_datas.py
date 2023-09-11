8b317d543c7f3e84a3e9862d17eecfde533e4224
HappyFaceFriend <yjl980730@gmail.com>
2023-09-11 15:03:59 +0900

    Added pose estimation script

--- 01_merge_datas.py
+++ 01_merge_datas.py
@@ -1,5 +1,36 @@
 #merge images from different sources to one json file, removing duplicates and discrepancies
 
+import os
+
+from PIL import Image
+from tqdm import tqdm
+
+import myutils.file_utils as file
+
+
+image_dir = '01_merged_datas/images/'
+target_dir = '01_merged_datas/images_resized/'
+
+
+def process_image(input_path, output_path, target_size=(512, 512), target_ext = 'JPEG'):
+    try:
+        with Image.open(input_path) as img:
+            img = img.convert("RGB")
+            img.thumbnail(target_size)
+            img.save(output_path, "JPEG")
+    except Exception as e:
+        print(f"Error processing {input_path}: {e}")
+
+if not os.path.exists(target_dir):
+    os.makedirs(target_dir)
+    
+all_file_names = file.get_file_names_in_dir(image_dir)
+for file_name in tqdm(all_file_names):
+    process_image(image_dir + file_name, target_dir + f"{os.path.splitext(file_name)[0]}.jpg")
+
+
+
+'''
 import pandas as pd
 
 import myutils.file_utils as file
@@ -7,8 +38,8 @@ import myutils.file_utils as file
 image_dir = '01_merged_datas/images/'
 output_csv_path = '01_merged_datas/image_names.csv'
 
-
 all_file_names = file.get_file_names_in_dir(image_dir)
 
 file_names_df = pd.DataFrame(all_file_names)
 file_names_df.to_csv(output_csv_path, header = ['name'], index = False)
+'''
---
+++ 02_pose_estimation_mth.py
@@ -0,0 +1,53 @@
+'''
+1. pct/env2/Scripts/activate
+2. cd pct
+3. python ../02_pose_estimation.py
+'''
+
+from tqdm import tqdm
+import pandas as pd
+from multiprocessing import Process
+
+from pct.vis_tools.run_keypoints import inference
+
+import myutils.file_utils as file
+
+
+image_dir = '../01_merged_datas/images_resized/'
+#output_csv_path = '../01_merged_datas/image_datas.csv'
+
+slice_size = 100
+num_workers = 2
+
+
+all_file_names = file.get_file_names_in_dir(image_dir)
+slice_idxs = [i for i in range(0, len(all_file_names), slice_size)]
+slice_idxs[-1] = len(all_file_names)
+
+
+def run_slice(offset):
+    for i in range(offset, len(slice_idxs), num_workers):
+        output_csv_path = f'../01_merged_datas/image_datas{i}.csv'
+        
+        print(f"Slice {i} START!")
+        pose_results = inference(all_file_names[slice_idxs[i] : slice_idxs[i+1]], image_dir)
+
+        print(f"Slice {i} DONE! ({len(pose_results)})")
+
+        pose_results_df = pd.DataFrame(pose_results)
+        pose_results_df.to_csv(output_csv_path, header = ['name', 'person_count', 'bbox', 'keypoints',])
+
+        print(f"Saved data as {output_csv_path}")
+
+
+
+if __name__ == '__main__':
+
+
+    processes = []
+    for i in range(num_workers):
+        processes.append(Process(target=run_slice, args=(i,)))
+        processes[i].start()
+
+    for i in range(num_workers):
+        processes[i].join()
